57077e468c66204e2a005342f0704720f764e425
S2ShiroeS2 <tuquochung123@gmail.com>
2021-04-13 18:15:15 +0700

    Revert "Merge pull request #3 from S2ShiroeS2/component/Header"
    This reverts commit d9c70617cf656da0582d6fa1d2ac074dd21c47de, reversing
    changes made to 6a5daaa02814a0e5b2d96e755ebb14101f10b768.

--- src/apis/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-const url = 'http://localhost:3000/products'
-
-export const fetchProducts = () => axios.get(url)
--- src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import productReducer from './products'
-
-import { combineReducers } from 'redux'
-
-const allReducers = combineReducers({
-    products: productReducer
-})
-
-export default allReducers
